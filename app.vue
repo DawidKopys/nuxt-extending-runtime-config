b0e7949541b85c8ba7ccf84a33d6aedc12037fc6
DawidKopys <dewke17@gmail.com>
2023-07-29 19:07:23 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Some text</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+console.log(runtimeConfig.);
+</script>
---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module 'nuxt/schema' {
+  interface RuntimeConfig {
+    foo: string
+  }
+}
+
+export {}
